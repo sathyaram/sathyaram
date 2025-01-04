02dd719f79b668c9918f971af931763edc7d62e4
Sathya Ram <sathyatheram@gmail.com>
2025-01-04 15:53:31 -0500

    new favicon

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
